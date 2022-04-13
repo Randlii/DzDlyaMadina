e0e39e4f52c2bd9b8ae6f7eb5d542bb7a82f461a
Randlii <93086288+Randlii@users.noreply.github.com>
2022-04-13 21:52:07 +0300

    Create dz2.1.cpp

---
+++ dz2.1.cpp
@@ -0,0 +1,39 @@
+{
+    std::string str;
+    std::cin >> str;
+    int i = 0;
+    int l;
+    int c = 0;
+    while(str[i] != '\0') {
+        if (str[i] == ')' || str[i] == '}' || str[i] == ']' || str[i] == '>') {
+            std::cout << i+1;
+            exit(0);
+        }
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[' || str[i] == '<') {
+            l = str[i];
+            c = i;
+            i++;
+            while (str[i] != '\0') {
+                if (str[i] == ')' || str[i] == '}' || str[i] == ']' || str[i] == '>') {
+                    if(str[i] == l +1 || str[i] == l +2)
+                        break;
+                    else
+                    {
+                        std::cout << c+1;
+                        exit(0);
+                        
+                    }
+                }
+           
+                if (str[i] == '(' || str[i] == '{' || str[i] == '[' || str[i] == '<' || str[i +1] == '\0')  {
+                    std::cout << c+1;
+                    exit(0);
+                    
+                }
+                i++;
+            }
+        }
+        i++;
+    }
+std::cout << "ошибок нет";
+}
