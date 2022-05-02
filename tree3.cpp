bbfa53f10210f48741fab4aa4a209259f9b51191
Randlii <93086288+Randlii@users.noreply.github.com>
2022-05-02 18:31:06 +0300

    Create tree3.cpp

---
+++ tree3.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+typedef struct TNode *PNode;
+struct TNode {    
+    string data;
+    PNode left;
+    PNode right;
+  };
+
+int Priority ( char op )
+{
+  switch ( op ) {
+    case '+':
+    case '-': return 1;
+    case '*':
+    case '/': return 2;
+    }
+  return 100;  
+}
+
+int LastOp ( string s )
+{
+  int i, minPrt, res;
+  minPrt = 50;
+  res = -1;
+  for ( i=0; i<s.size(); i++ )
+    if ( Priority(s[i]) <= minPrt ) {
+      minPrt = Priority(s[i]);
+      res = i;
+      }
+  return res;
+}
+
+PNode MTree ( string s )
+{
+  int k;
+  PNode Tree; 
+  Tree = new struct TNode;
+  k = LastOp ( s );
+  if ( k == -1 ) {
+    Tree->data = s;
+    Tree->left = NULL;
+    Tree->right = NULL;
+    }
+  else {
+    Tree->data = s.substr ( k, 1);    
+    Tree->left = MTree ( s.substr(0,k)  );
+    Tree->right = MTree ( s.substr(k+1) );
+}
+  return Tree;
+}
+
+int Calc ( PNode Tree )
+{
+  int n1, n2, res;
+  if ( Tree->left == NULL )
+    res = atoi(Tree->data.c_str());
+  else {
+    n1 = Calc ( Tree->left );
+    n2 = Calc ( Tree->right );
+    switch ( Tree->data[0] ) {
+      case '+': res = n1 + n2; break;
+      case '-': res = n1 - n2; break;
+      case '*': res = n1 * n2; break;
+      case '/': res = n1 / n2; break;
+      default: res = 99999;
+    }    
+  }
+  return res;
+}
+void treeprint(PNode tree) {
+  if (tree!=NULL) {
+    cout << tree->data[0]; 
+    treeprint(tree->left); 
+    treeprint(tree->right); 
+  }
+}
+void treeprintpost(PNode tree) {
+  if (tree!=NULL) { 
+    treeprintpost(tree->left); 
+    treeprintpost(tree->right); 
+    cout << tree->data[0];
+  }
+}
+void shirina(PNode root)
+{
+    cout << "\nОбход дерева в ширину:";
+
+    queue <PNode> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+
+        PNode f = q.front();
+        q.pop();
+
+        if (f == NULL)
+        {
+            if (!q.empty())
+                q.push(NULL);
+        }
+        else
+        {
+            cout << f->data[0] << " ";
+            if (f->left)
+                q.push(f->left);
+            if (f->right)
+                q.push(f->right);
+        }
+    }
+}
+int main()
+{
+  PNode Tree1;
+  string s;   
+  cout << "Введите выражение: \n";
+  cin >> s;
+  Tree1 = MTree ( s );
+    cout << "Префиксная фома:\n";
+  treeprint(Tree1);
+  cout << "\nПостфиксная форма:\n";
+  treeprintpost(Tree1);
+  shirina(Tree1);
+  cout << "\n" << "отв: " << Calc(Tree1); 
+}
